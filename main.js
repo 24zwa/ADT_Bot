795757e70bd5a8f2b2e32195c9162f488c0d4cf0
24zwa <nishizawa.akihiro@classmethod.jp>
2023-07-01 17:02:00 +0900

    use attachment

--- main.js
+++ main.js
@@ -3,88 +3,114 @@ const GOOGLE_CALENDER_ID = propertie.GOOGLE_CALENDER_ID;
 const SLACK_OAUTH_TOKEN = propertie.SLACK_OAUTH_TOKEN;
 const SLACK_CHANNEL = propertie.SLACK_CHANNEL;
 
-// Slack への通知設定
-function notifyToSlack(message) {
-    let payload = {
-        token: SLACK_OAUTH_TOKEN,
-        channel: SLACK_CHANNEL,
-        text: message,
-    };
-    let params = {
-        method: "post",
-        payload: payload,
-    };
-    const postUrl = "https://slack.com/api/chat.postMessage";
-    UrlFetchApp.fetch(postUrl, params);
-}
-
-// [日次] 勤怠登録を ADT チャンネルに通知
 function getDailyEvent() {
     const today = new Date();
     const dailyEvents = CalendarApp.getCalendarById(GOOGLE_CALENDER_ID).getEventsForDay(today);
 
-    const notifyDailyMessage = ':calendar: 本日の勤怠登録をお知らせします :calendar:';
-    notifyToSlack(notifyDailyMessage);
+    callDailyFunction();
 
-    if (dailyEvents.length == 0) {
-        const noEventsInDay = '本日の勤怠登録は、ありません';
-        notifyToSlack(noEventsInDay);
-        return;
+    function callDailyFunction() {
+        let message = "";
+        if (dailyEvents.length == 0) {
+            message = ":calendar: 本日のイベント登録は、ありません :calendar:";
+        } else {
+            message = `:calendar: 本日のイベント登録は、${dailyEvents.length} 件です :calendar:`;
+        }
+
+        const attachments = makeAttachmentsMessageForWeekly(dailyEvents);
+        postMessageToSlack(message, attachments);
     }
 
-    for (let i in dailyEvents) {
-        let title = dailyEvents[i].getTitle();
+    function makeAttachmentsMessageForWeekly(dailyEvents) {
+        return dailyEvents.map(function (event, index) {
+            if (dailyEvents[index].isAllDayEvent()) {
+                const title = event.getTitle();
+                let startAllDay = dailyEvents[index].getAllDayStartDate();
+                let endAllDay = dailyEvents[index].getAllDayEndDate();
+                startAllDay = Utilities.formatDate(startAllDay, "JST", "yyyy/MM/dd");
+                endAllDay = Utilities.formatDate(endAllDay, "JST", "yyyy/MM/dd");
+                const text = startAllDay + ' 〜 ' + endAllDay;
 
-        if (dailyEvents[i].isAllDayEvent()) {
-            let startAllDay = dailyEvents[i].getAllDayStartDate();
-            let endAllDay = dailyEvents[i].getAllDayEndDate();
-            startAllDay = Utilities.formatDate(startAllDay, "JST", "yyyy/MM/dd");
-            endAllDay = Utilities.formatDate(endAllDay, "JST", "yyyy/MM/dd");
-            let postAllDayMessage = '・' + title + '  期間: ' + startAllDay + ' 〜 ' + endAllDay;
-            notifyToSlack(postAllDayMessage);
-        } else {
-            let startTime = dailyEvents[i].getStartTime();
-            let endTime = dailyEvents[i].getEndTime();
-            startTime = Utilities.formatDate(startTime, "JST", "yyyy/MM/dd HH:mm");
-            endTime = Utilities.formatDate(endTime, "JST", "yyyy/MM/dd HH:mm");
-            let postMessage = '・' + title + '  期間: ' + startTime + ' 〜 ' + endTime;
-            notifyToSlack(postMessage);
-        }
+                return new attachmentBody(title, text)
+            } else {
+                const title = event.getTitle();
+                let startTime = dailyEvents[index].getStartTime();
+                let endTime = dailyEvents[index].getEndTime();
+                startTime = Utilities.formatDate(startTime, "JST", "yyyy/MM/dd HH:mm");
+                endTime = Utilities.formatDate(endTime, "JST", "yyyy/MM/dd HH:mm");
+                const text = startTime + ' 〜 ' + endTime;
+
+                return new attachmentBody(title, text)
+            }
+        })
     }
 }
 
-// [週次] 勤怠登録を ADT チャンネルに通知
 function getWeeklyEvent() {
     const now = new Date();
     const end = new Date(now.getTime() + (7 * 24 * 60 * 60 * 1000)); // 1週間後
     const weeklyEvents = CalendarApp.getCalendarById(GOOGLE_CALENDER_ID).getEvents(now, end);
 
-    const notifyWeeklyMessage = ':calendar: 今週の勤怠登録をお知らせします :calendar:';
-    notifyToSlack(notifyWeeklyMessage);
+    callWeeklyFunction();
 
-    if (weeklyEvents.length == 0) {
-        const noEventsInWeek = '今週の勤怠登録は、現時点でありません';
-        notifyToSlack(noEventsInWeek);
-        return;
-    }
-
-    for (let i in weeklyEvents) {
-        let title = weeklyEvents[i].getTitle();
-
-        if (weeklyEvents[i].isAllDayEvent()) {
-            let startAllDay = weeklyEvents[i].getAllDayStartDate();
-            let endAllDay = weeklyEvents[i].getAllDayEndDate();
-            startAllDay = Utilities.formatDate(startAllDay, "JST", "yyyy/MM/dd");
-            endAllDay = Utilities.formatDate(endAllDay, "JST", "yyyy/MM/dd");
-            let postAllDayMessage = '・' + title + '  期間: ' + startAllDay + ' 〜 ' + endAllDay;
-            notifyToSlack(postAllDayMessage);
+    function callWeeklyFunction() {
+        let message = "";
+        if (weeklyEvents.length == 0) {
+            message = ":spiral_calendar_pad: 今週のイベント登録は、ありません :spiral_calendar_pad: ";
         } else {
-            let startTime = weeklyEvents[i].getStartTime();
-            let endTime = weeklyEvents[i].getEndTime();
-            startTime = Utilities.formatDate(startTime, "JST", "yyyy/MM/dd HH:mm");
-            endTime = Utilities.formatDate(endTime, "JST", "yyyy/MM/dd HH:mm");
-            let postMessage = '・' + title + '  期間: ' + startTime + ' 〜 ' + endTime;
-            notifyToSlack(postMessage);
+            message = `:spiral_calendar_pad: 今週のイベント登録は、${weeklyEvents.length} 件です :spiral_calendar_pad:`;
         }
+
+        const attachments = makeAttachmentsMessageForWeekly(weeklyEvents);
+        postMessageToSlack(message, attachments);
     }
+
+    function makeAttachmentsMessageForWeekly(weeklyEvents) {
+        return weeklyEvents.map(function (event, index) {
+            if (weeklyEvents[index].isAllDayEvent()) {
+                const title = event.getTitle();
+                let startAllDay = weeklyEvents[index].getAllDayStartDate();
+                let endAllDay = weeklyEvents[index].getAllDayEndDate();
+                startAllDay = Utilities.formatDate(startAllDay, "JST", "yyyy/MM/dd");
+                endAllDay = Utilities.formatDate(endAllDay, "JST", "yyyy/MM/dd");
+                const text = startAllDay + ' 〜 ' + endAllDay;
+
+                return new attachmentBody(title, text)
+            } else {
+                const title = event.getTitle();
+                let startTime = weeklyEvents[index].getStartTime();
+                let endTime = weeklyEvents[index].getEndTime();
+                startTime = Utilities.formatDate(startTime, "JST", "yyyy/MM/dd HH:mm");
+                endTime = Utilities.formatDate(endTime, "JST", "yyyy/MM/dd HH:mm");
+                const text = startTime + ' 〜 ' + endTime;
+
+                return new attachmentBody(title, text)
+            }
+        })
+    }
+}
+
+class attachmentBody {
+    constructor(title, text) {
+        this.title = title
+        this.text = text
+        this.color = "#36a64f"
+    }
+}
+
+function postMessageToSlack(message, attachments) {
+    const payload = {
+        "token": SLACK_OAUTH_TOKEN,
+        "channel": SLACK_CHANNEL,
+        "text": message,
+        "attachments": JSON.stringify(attachments)
+    };
+
+    const params = {
+        "method": "POST",
+        "payload": payload
+    }
+
+    const postUrl = "https://slack.com/api/chat.postMessage";
+    UrlFetchApp.fetch(postUrl, params)
 }
